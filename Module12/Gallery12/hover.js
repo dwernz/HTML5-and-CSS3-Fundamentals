8c19921d8a1721d107163a6f700e3e48d3083f25
Daniel <62781672+dwernz@users.noreply.github.com>
2022-11-30 10:33:50 -0500

    Finished HTML5 and CSS3 Fundamentals Course!

---
+++ Module12/Gallery12/hover.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#gallery a').hover(function() {
+        var galleryHref = $(this).attr('href');
+        var galleryAlt = $(this).attr('title');
+        $('figure img').attr({src: galleryHref, alt: galleryAlt});
+        $('figcaption').html(galleryAlt);
+    });
+});
